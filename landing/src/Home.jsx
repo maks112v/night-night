032c976d84c4287e8aef32549df8a7403594fffb
Maksim V <maks112v@gmail.com>
2019-03-03 15:17:00 -0500

    updates to version 1.0.5

--- landing/src/Home.jsx
+++ landing/src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./Components/Home/Header";
 import Features from "./Components/Home/Features";
 import { MDBContainer } from "mdbreact";
